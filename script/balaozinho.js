c9fd605d2735d26e28b206ead6c877be9e88df22
IagorSs <iagor.penaranda@uol.com.br>
2021-12-12 11:22:02 -0300

    Done Exercice 1

---
+++ script/balaozinho.js
@@ -0,0 +1,28 @@
+const balaozinho = document.getElementById('balaozinho');
+
+const marcacoes = document.getElementsByClassName('marcacao');
+
+for(const marcacao of marcacoes) {
+  const titulo = document.createElement('h2');
+  titulo.textContent = marcacao.attributes.getNamedItem('data-titulo').value;
+
+  const conteudo = document.createElement('p');
+  conteudo.textContent = marcacao.attributes.getNamedItem('data-conteudo').value;
+
+  const textColor = marcacao.attributes.getNamedItem('data-cor').value;
+
+  marcacao.addEventListener('mouseover', () => {
+    balaozinho.appendChild(titulo);
+    balaozinho.appendChild(conteudo);
+    balaozinho.style.color = textColor;
+  });
+
+  marcacao.addEventListener('mouseout', () => {
+    balaozinho.innerHTML = '';
+  });
+
+  marcacao.addEventListener('mousemove', ({ pageX, pageY }) => {
+    balaozinho.style.top = `${pageY}px`;
+    balaozinho.style.left = `${pageX}px`;
+  })
+}
