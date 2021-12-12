ffc8a0c3abb95c2b0683b9c8482bf80d394539ba
IagorSs <iagor.penaranda@uol.com.br>
2021-12-12 17:47:09 -0300

    Done Challenge 1

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -4,3 +4,15 @@ const filtroDaFoto = document.getElementById('filtro-da-foto');
 filtroDaFoto.onchange = ({ target: { value } }) => {
   image.style.filter = value;
 }
+
+const imageSelect = document.getElementById('imagem');
+imageSelect.addEventListener('change', ({ target: { files } }) => {
+  const reader = new FileReader();
+  const [file] = files;
+
+  reader.addEventListener('load', ({ target: { result } }) => {
+    image.src = result;
+  });
+
+  reader.readAsDataURL(file);
+})
